db48b87f833a36b2e4d0176a0219bc4b94aafc4d
busratok <busratokuc@gmail.com>
2024-03-13 12:41:17 +0000

    errorHandler is added

--- index.js
+++ index.js
@@ -8,8 +8,12 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
 
+app.use(express.json()); // keep it at the top
+
 app.all("/", (req, res) => {
   res.send("Hello");
 });
+
+app.use("./src/errorHandler"); // keep it at the bottom
 
 app.listen(PORT, () => console.log(`Running on: http://${HOST}:${PORT}`));
