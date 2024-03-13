43f368937ee148a34444761040a13feae4a1a00f
busratok <busratokuc@gmail.com>
2024-03-13 22:11:08 +0000

    user model is created

---
+++ src/models/user.model.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    firstName: String,
+    lastName: String,
+  },
+  {
+    collection: "user",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
