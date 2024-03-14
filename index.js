de4e9e4d623760922a24b6448603993ee48bc7aa
busratok <busratokuc@gmail.com>
2024-03-14 11:14:14 +0000

    cooki-session is installed and added to the app

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const HOST = process.env.HOST;
 // DB
 require("./src/configs/dbConnection");
 
+/* ------------------------------------------------------- */
+// SessionCookies:
+// http://expressjs.com/en/resources/middleware/cookie-session.html
+// https://www.npmjs.com/package/cookie-session
+//* $ npm i cookie-session
+
+const session = require("cookie-session");
+
+app.use(
+  session({
+    secret: process.env.SECRET_KEY, //secret key
+    // maxAge: 1000 * 60 * 60 * 24 *3, // 3 days
+  })
+);
+
 app.use(express.json()); // keep it at the top
 
 app.all("/", (req, res) => {
