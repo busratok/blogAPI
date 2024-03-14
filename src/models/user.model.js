58694243ce4291b82fd3b509570d5c9544956395
busratok <busratokuc@gmail.com>
2024-03-14 10:17:18 +0000

    password encrption function is created and use for password field in the userModel

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,6 +2,18 @@
 
 const mongoose = require("mongoose");
 
+//Password Encryption
+const crypto = require("node:crypto");
+
+const keyCode = process.env.SECRET_KEY || "write_random_chars_in_here";
+const loopCount = 10_000; // 10K
+const charCount = 32; // assign 32 for 64
+const encType = "sha512";
+
+const passwordEncrypt = function (password) {
+  return crypto.pbkdf2Sync(password, keyCode, loopCount, charCount, encType);
+};
+
 const UserSchema = new mongoose.Schema(
   {
     email: {
@@ -19,6 +31,8 @@ const UserSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
+      // set: (password) => passwordEncrypt(password),
+      set: passwordEncrypt,
     },
     firstName: String,
     lastName: String,
